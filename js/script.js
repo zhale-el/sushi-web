27686c47ecb88b85c362e775249382e325c60b2c
zhaleh <zhalehelmi@gmail.com>
2023-08-14 11:59:21 +0330

    js scrollheader and css

--- js/script.js
+++ js/script.js
@@ -23,9 +23,24 @@ const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
   const navMenu = document.getElementById("nav_menu");
+  //when we click on each nav__link, we remove the show_menu class
   navMenu.classList.remove("show-menu");
 };
 
 navLink.forEach((n) => {
   n.addEventListener("click", linkAction);
 });
+
+// < ====== change background Header ====== >
+
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  console.log(header);
+
+  // when the scroll is greater than 50 viewport height ,add the bg_header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add("bg_header")
+    : header.classList.remove("bg_header");
+};
+
+window.addEventListener("scroll", scrollHeader);
