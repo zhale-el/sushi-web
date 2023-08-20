fdfd32f4a03e24de8722da245d450b1ca90b20b5
zhaleh <zhalehelmi@gmail.com>
2023-08-20 17:03:15 +0330

    completed

--- js/script.js
+++ js/script.js
@@ -117,3 +117,18 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("selected-theme", getCurrentTheme());
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
+
+// < =======  scroll ======= >
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset:true //repeat
+});
+sr.reveal(`.home__img,
+.newsLetter__container, .footer__logo, .footer__description, .footer__content, .footer__info `);
+sr.reveal(`.home__data`, { origin: "bottom" });
+sr.reveal(`.about__data, .recently__data`, { origin: "left" });
+sr.reveal(`.about__img, .recently_img`, { origin: "right" });
+sr.reveal(`.popular__card`, { interval: 100 });
